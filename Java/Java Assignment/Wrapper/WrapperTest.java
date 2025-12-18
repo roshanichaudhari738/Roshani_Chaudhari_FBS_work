6ae5464361fc9b6a292386bd18567918b0becf74
roshanichaudhari738 <158252754+roshanichaudhari738@users.noreply.github.com>
2025-12-18 11:43:52 +0530

    Add files via upload

---
+++ Java/Java Assignment/Wrapper/WrapperTest.java
@@ -0,0 +1,18 @@
+package Practise;
+
+public class WrapperTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int b=10;
+		Integer a=10;
+		int c=a;//unboxing Wrapper to Primitive
+		System.out.println(c);
+		
+		int a1=11;
+		Integer a2;
+		a2=a1;//AutoBoxing Primitive to Wrapper
+		System.out.println(a2);
+	}
+
+}
