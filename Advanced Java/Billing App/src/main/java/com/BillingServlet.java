3df41438400da94108797452ccf87d32219de35a
roshanichaudhari738 <158252754+roshanichaudhari738@users.noreply.github.com>
2026-02-21 18:52:13 +0530

    Billing App using servlet ,html

---
+++ Advanced Java/Billing App/src/main/java/com/BillingServlet.java
@@ -0,0 +1,22 @@
+package com;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class BillingServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			PrintWriter out=response.getWriter();
+			out.println("<a href='log'>Logout</a>");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+}
---
+++ Advanced Java/Billing App/src/main/java/com/Logout.java
@@ -0,0 +1,29 @@
+package com;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Logout extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			PrintWriter out=response.getWriter();
+			out.println("Exit");
+			HttpSession s=request.getSession();
+			s.invalidate();
+			response.sendRedirect("http://localhost:8080/WebApplication/LoginPage.html");
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
